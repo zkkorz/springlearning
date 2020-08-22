b545de040000c42b6dd7c5affa1f2a49700957ea
zhangkaikai <zkkorz@outlook.com>
2020-08-22 15:46:52 +0800

    Spring Data Binder

---
+++ iocoverview/src/main/java/com/spring/learning/iocoverview/domain/Company.java
@@ -0,0 +1,21 @@
+package com.spring.learning.iocoverview.domain;
+
+public class Company {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
